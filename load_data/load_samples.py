89c2df721be565a430a51af98a06845175d2adc0
airpod2 <thvldk40@naver.com>
2022-02-26 18:02:00 +0900

    sample data load

--- load_data/load_samples.py
+++ load_data/load_samples.py
@@ -1,8 +1,82 @@
 import os
+import bcrypt
 import pymysql
 import dotenv 
+import pandas as pd
 
 dotenv.load_dotenv()
+
+#타입 확인
+# print(type(int(os.environ.get('MYSQL_PORT'))))
+
+#Connecting to the database
+conn = pymysql.connect(host='127.0.0.1',
+					   port=int(os.getenv('MYSQL_PORT')), 
+					   user=os.getenv('MYSQL_USER'),
+ 					   password=os.getenv('MYSQL_PASSWORD'), 
+					   db=os.getenv('MYSQL_DATABASE'), 
+					   charset='utf8') 
+
+# Creating a cursor object to execute
+cur = conn.cursor()
+
+'''기존 데이터를 모두 지우고 AUTO_INCREMENT도 초기화 한다.'''
+cur.execute('''show tables''')
+tables = cur.fetchall() #튜플 형태
+
+# cur.execute('''SET foreign_key_checks = 0''') #외래키 해제 후 테이블 삭제
+for table in tables:
+	cur.execute(f"DELETE FROM {table[0]}")
+	cur.execute(f"ALTER TABLE {table[0]} AUTO_INCREMENT = 1")
+# cur.execute('''SET foreign_key_checks = 1''') #외래키 해제 다시 복구
+
+'''User 테이블에 id 1번으로 admin 유저 넣기'''
+encrypted_pw = bcrypt.hashpw(os.getenv('ADMIN_USER_PASSWORD').encode('utf-8'),bcrypt.gensalt())
+cur.execute('''INSERT INTO user (email, password, nickname) VALUES(%s,%s,%s)''', [os.getenv('ADMIN_USER_EMAIL'), encrypted_pw, "admin_Maronie"])
+
+# Opening csv files
+df_classification = pd.read_csv('classification_sample.csv',keep_default_na=False) #keep_default_na=False : NaN->None로 바꾸기. None은 db에서 null로 인식된다
+df_liquor = pd.read_csv('liquor_sample.csv',keep_default_na=False)
+df_cocktail = pd.read_csv('cocktail_sample.csv',keep_default_na=False)
+df_byLiquor = pd.read_csv('by_liquor_sample.csv',keep_default_na=False)
+df_menu = pd.read_csv('menu_sample.csv',keep_default_na=False)
+df_paring = pd.read_csv('paring_sample.csv',keep_default_na=False)
+
+'''classification'''
+for row in df_classification.itertuples():
+		cur.execute('''INSERT INTO classification (classification) VALUES(%s)''', [row.classification])
+
+'''liquor'''
+for row in df_liquor.itertuples():
+		cur.execute('''INSERT INTO liquor (liquor_name, classification_id, alcohol, price, image_path, description, rating, vendor) 
+		VALUES(%s,%s,%s,%s,%s,%s,%s,%s)''', [row.liquor_name, row.classification_id, row.alcohol, row.price, row.image_path, row.description, row.rating, row.vendor])
+
+'''cocktail'''
+#author_id, heart는 default value로 한다.
+for row in df_cocktail.itertuples():
+		cur.execute('''INSERT INTO cocktail (cocktail_name, alcohol, image_path, description, level, ingredients, recipe) 
+		VALUES(%s,%s,%s,%s,%s,%s,%s)''', [row.cocktail_name, row.alcohol, row.image_path, row.description, row.level, row.ingredients, row.recipe])
+
+'''by_liquor'''
+for row in df_byLiquor.itertuples():
+		cur.execute('''INSERT INTO by_liquor (classification_id, cocktail_id) 
+		VALUES(%s, %s)''', [row.classification_id, row.cocktail_id])
+
+'''menu'''
+for row in df_menu.itertuples():
+		cur.execute('''INSERT INTO menu (menu_name, image_path) 
+		VALUES(%s, %s)''', [row.menu_name, row.image_path])
+
+'''paring'''
+for row in df_paring.itertuples():
+		cur.execute('''INSERT INTO paring (menu_id, classification_id) 
+		VALUES(%s, %s)''', [row.menu_id, row.classification_id])
+
+#Committing the changes
+conn.commit()
+
+#closing the database con
+conn.close()
 
 '''
 진: 고든스 진, 봄베이 사파이어
@@ -11,33 +85,3 @@ dotenv.load_dotenv()
 보드카: 앱솔루트, 스미노프
 데킬라: 호세쿠엘보
 '''
-#타입 확인
-# print(type(int(os.environ.get('MYSQL_PORT'))))
-
-conn = pymysql.connect(host='127.0.0.1',
-					   port=int(os.getenv('MYSQL_PORT')), 
-					   user=os.getenv('MYSQL_USER'),
- 					   password=os.getenv('MYSQL_PASSWORD'), 
-					   db=os.getenv('MYSQL_DATABASE'), 
-					   charset='utf8') 
-cur = conn.cursor()
-
-#기존 데이터를 모두 지우고 AUTO_INCREMENT도 초기화 한다.
-cur.execute('''DELETE FROM liquor''')
-cur.execute('''DELETE FROM classification''')
-cur.execute('''ALTER TABLE classification AUTO_INCREMENT = 1''')
-cur.execute('''ALTER TABLE liquor AUTO_INCREMENT = 1''')
-cur.execute('''INSERT INTO classification (classification) VALUES("Liqueur")''')
-
-#Committing the changes
-conn.commit()
-
-cur.execute('''INSERT INTO liquor (liquor_name, classification_id, image_path, rating, description) VALUES(%s,%s,%s,%s,%s)''', 
-	["X-Rated", 1, "https://file.mk.co.kr/meet/neds/2020/08/image_readtop_2020_812159_15967844304309365.jpg", 4.5, "abc"])
-
-#Committing the changes
-conn.commit()
-
-#closing the database con
-conn.close()
-
