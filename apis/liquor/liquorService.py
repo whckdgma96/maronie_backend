53f8d06cf25164479695cd2a6f5d32dc0115d372
airpod2 <thvldk40@naver.com>
2022-02-27 18:25:15 +0900

    load data 삭제(불필요) 및 술, 칵테일을 이름과 id 각각 조회 가능하도록 수정

--- apis/liquor/liquorService.py
+++ apis/liquor/liquorService.py
@@ -16,23 +16,36 @@ from models.liquor import Cocktail
 #     else: 
 #         return {"message":"Not found"},404
 
-def liquor_detail_view(liquor_name:str):
-    liquor = Liquor.query.filter(Liquor.liquor_name.like(liquor_name)).first()
+def liquor_detail_view(liquor_id:int):
+    liquor = Liquor.query.filter_by(id = liquor_id).first()
     if liquor:
         return {
             "liquor_name":liquor.liquor_name,
             # "classification_id":liquor.classification_id,
-            # "rating" : liquor.rating,
+            "rating" : liquor.rating,
             # "description" : liquor.description
         },200 #성공
     else: 
         return {"message":"Not found"},404
 
-def cocktail_detail_view(cocktail_name:str):
-    cocktail = Cocktail.query.filter_by(cocktail_name=cocktail_name).first()
+def liquor_search_text(liquor_name:str):
+    liquor = Liquor.query.filter(Liquor.liquor_name.like('%'+liquor_name+'%')).first()
+    if liquor:
+        return {
+            "liquor_name":liquor.liquor_name,
+            # "classification_id":liquor.classification_id,
+            "rating" : liquor.rating,
+            # "description" : liquor.description
+        },200 #성공
+    else: 
+        return {"message":"Not found"},404
+
+def cocktail_detail_view(cocktail_id:int):
+    cocktail = Cocktail.query.filter_by(id=cocktail_id).first()
     if cocktail:
         return {
             "id":cocktail.id,
+            "name" : cocktail.cocktail_name,
             "ingredient":cocktail.ingredients,
             "recipe" : cocktail.recipe,
             "level":cocktail.level,
@@ -40,4 +53,17 @@ def cocktail_detail_view(cocktail_name:str):
         },200 #성공
     else: 
         return {"message":"Not found"},404
-    
+
+def cocktail_search_text(cocktail_name:str):
+    cocktail = Cocktail.query.filter(Cocktail.cocktail_name.like('%'+cocktail_name+'%')).first()
+    if cocktail:
+        return {
+            "id":cocktail.id,
+            "name" : cocktail.cocktail_name,
+            "ingredient":cocktail.ingredients,
+            "recipe" : cocktail.recipe,
+            "level":cocktail.level,
+            "description" : cocktail.description
+        },200 #성공
+    else: 
+        return {"message":"Not found"},404
