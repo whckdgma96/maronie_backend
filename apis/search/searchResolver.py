18abbb20774c5df3f6cdd55154444bf0a1d97c44
airpod2 <thvldk40@naver.com>
2022-03-02 21:14:20 +0900

    mask 필드 비활성화 및 pandas 사용 안하는 경우

--- apis/search/searchResolver.py
+++ apis/search/searchResolver.py
@@ -9,7 +9,7 @@ from .searchDTO import *
 class search_keyword(Resource):
     @Search.response(200, "success", result_text_response)
     @Search.response(500, "fail")
-    @Search.marshal_with(result_text_response)
+    @Search.marshal_with(result_text_response, mask=False)
     def get(self,keyword):
         '''술/칵테일 이름으로 정보 조회'''
         return searchService.search_keyword(keyword)
