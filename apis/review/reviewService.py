91ed9954effa305cced0d23b01657d4868a5d28e
CHC <whckdgma96@gmail.com>
2022-03-06 14:09:26 +0900

    리뷰 수정

--- apis/review/reviewService.py
+++ apis/review/reviewService.py
@@ -10,11 +10,10 @@ def create_review(user_id:int,liquor_id:int,rating:float,content:str):
     # print(review_date)
     review_check = Review.query.filter_by(user_id=user_id).filter_by(liquor_id=liquor_id).first()
     if review_check: # 있는 리뷰 -> 업데이트를 해야댐
-        return{'message':'얘는 업데이트를 해야합니다. TODO'},201
+        abort(500, "이미 리뷰를 등록했습니다")
     else: 
         new_review = Review(user_id = user_id,liquor_id=liquor_id,rating=rating,content=content,review_date=review_date)
         db.session.add(new_review)
         db.session.commit()
         return {"message":"리뷰등록 성공"},200
-
 
