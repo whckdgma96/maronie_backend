df60764b523ca3f379860f8276e970ae2373bae2
airpod2 <thvldk40@naver.com>
2022-03-06 16:18:18 +0900

    status code와 메세지 수정

--- apis/review/reviewResolver.py
+++ apis/review/reviewResolver.py
@@ -1,18 +1,39 @@
 from flask_restx import Resource
 from .reviewDTO import *
 from . import reviewService
-from flask import request
-
+from flask import request, session,abort
+from models.user import User
 # 리쿼 리뷰 생성
 @Review.route('/create')
 class Create_review(Resource):
     @Review.expect(create_reviewDTO)
-    @Review.response(200, 'Successfully created a review')
-    @Review.response(500, 'Failure creating a review')
+    @Review.response(201, '리뷰등록 성공')
+    @Review.response(500, 'Failed to create a review')
     def post(self):
         '''술 리뷰 생성'''
-        user_id = request.json['user_id']
-        liquor_id = request.json['liquor_id']
-        rating = request.json['rating']
-        content = request.json['content']
-        return reviewService.create_review(user_id,liquor_id,rating,content)
+        try:
+            logined_user = User.query.filter_by(email=session['login']).first()
+            user_id = logined_user.id
+            liquor_id = request.json['liquor_id']
+            rating = request.json['rating']
+            content = request.json['content']
+            return reviewService.create_review(user_id,liquor_id,rating,content)
+        except:
+            abort(500, "리뷰 등록 실패.")
+
+@Review.route('/update')
+class UpdateReview(Resource):
+    @Review.expect(update_reviewDTO)
+    @Review.response(200, 'Review revision successful')
+    @Review.response(500, 'Failed to revise the review')
+    def post(self):
+        '''술 리뷰 수정'''
+        try:
+            logined_user = User.query.filter_by(email=session['login']).first()
+            user_id = logined_user.id
+            liquor_id = request.json['liquor_id']
+            rating = request.json['rating']
+            content = request.json['content']
+            return reviewService.update_review(user_id,liquor_id,rating,content)
+        except:
+            abort(500, "로그인 해주세요.")
--- apis/review/reviewService.py
+++ apis/review/reviewService.py
@@ -5,15 +5,33 @@ from models.user import *
 from db_connect import db
 from datetime import datetime
 
-
 def create_review(user_id:int,liquor_id:int,rating:float,content:str):
+    
+    
     review_date = datetime.today().strftime("%Y-%m-%d")
     # print(review_date)
     review_check = Review.query.filter_by(user_id=user_id).filter_by(liquor_id=liquor_id).first()
-    if review_check: # 있는 리뷰 -> 업데이트를 해야댐
+    if not session:
+        abort(500, "로그인 해주세요.")
+    elif review_check: # 있는 리뷰 -> 업데이트를 해야댐
         abort(500, "이미 리뷰를 등록했습니다")
     else: 
         new_review = Review(user_id = user_id,liquor_id=liquor_id,rating=rating,content=content,review_date=review_date)
         db.session.add(new_review)
         db.session.commit()
-        return {"message":"리뷰등록 성공"},200
+        return {"message":"리뷰등록 성공"},201
+
+def update_review(user_id:int,liquor_id:int,rating:float,content:str):
+    conn = pymysql.connect(host='127.0.0.1',port=3306, user='team11', password='AIteam11Liquor', db='liquor', charset='utf8') #숨기기
+    cur = conn.cursor()
+    sql = """UPDATE review SET rating=%s, content=%s, review_date=%s WHERE user_id=%s AND liquor_id=%s"""
+    review_date = datetime.today().strftime("%Y-%m-%d")
+    review_check = Review.query.filter_by(user_id=user_id).filter_by(liquor_id=liquor_id).first()
+    if not review_check:
+        abort(500, "리뷰등록을 해주세요.")
+    
+    else:
+        cur.execute(sql,(rating,content,review_date,user_id,liquor_id))
+        conn.commit()
+        return {"message":"리뷰수정 성공"},200
+    
