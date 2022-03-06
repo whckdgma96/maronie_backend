fdc863a122c3e7d65edd7d278abf79627ac1857e
CHC <whckdgma96@gmail.com>
2022-03-06 14:55:36 +0900

    세션(로그인여부)에 따른 리뷰등록

--- apis/review/reviewResolver.py
+++ apis/review/reviewResolver.py
@@ -1,8 +1,8 @@
 from flask_restx import Resource
 from .reviewDTO import *
 from . import reviewService
-from flask import request
-
+from flask import request, session,abort
+from models.user import User
 # 리쿼 리뷰 생성
 @Review.route('/create')
 class Create_review(Resource):
@@ -11,8 +11,12 @@ class Create_review(Resource):
     @Review.response(500, 'Failure creating a review')
     def post(self):
         '''술 리뷰 생성'''
-        user_id = request.json['user_id']
-        liquor_id = request.json['liquor_id']
-        rating = request.json['rating']
-        content = request.json['content']
-        return reviewService.create_review(user_id,liquor_id,rating,content)
+        try:
+            logined_user = User.query.filter_by(email=session['login']).first()
+            user_id = logined_user.id
+            liquor_id = request.json['liquor_id']
+            rating = request.json['rating']
+            content = request.json['content']
+            return reviewService.create_review(user_id,liquor_id,rating,content)
+        except:
+            abort(500, "리뷰 등록 실패.")
