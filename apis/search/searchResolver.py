e5c44b154e9c4f3e107d5df2ec044ce8f75e09f6
airpod2 <thvldk40@naver.com>
2022-03-08 14:31:46 +0900

    save model 형식으로 시도

--- apis/search/searchResolver.py
+++ apis/search/searchResolver.py
@@ -14,16 +14,16 @@ class search_keyword(Resource):
         '''술/칵테일 이름으로 정보 조회'''
         return searchService.search_keyword(keyword)
 
-# #술 이미지로 검색
-# @Search.route('/file-upload')
-# class search_image(Resource):
-#     @Search.expect(image_liquor)
-#     @Search.response(200, "success", result_image_response)
-#     @Search.response(500, "fail")
-#     @Search.marshal_with(result_image_response, mask=False)
-#     def post(self):
-#         '''술 이미지로 검색'''
-#         liquor_image = request.files
-#         return searchService.search_image(liquor_image=liquor_image)
+#술 이미지로 검색
+@Search.route('/file-upload')
+class search_image(Resource):
+    @Search.expect(image_liquor)
+    @Search.response(200, "success", result_image_response)
+    @Search.response(500, "fail")
+    @Search.marshal_with(result_image_response, mask=False)
+    def post(self):
+        '''술 이미지로 검색'''
+        liquor_image = request.files
+        return searchService.search_image(liquor_image=liquor_image)
 
 
