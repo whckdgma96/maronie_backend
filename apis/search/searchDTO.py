8d046285eb952d7a09d0541d512b74c6383e788a
airpod2 <thvldk40@naver.com>
2022-03-11 14:03:03 +0900

    텍스트 검색시 위시리스트 담긴 숫자 출력 추가

--- apis/search/searchDTO.py
+++ apis/search/searchDTO.py
@@ -3,6 +3,10 @@ from werkzeug.datastructures import FileStorage
 
 Search = Namespace(name="Search", description="술 라벨 이미지 검색 혹은 술과 칵테일 이름으로 정보 검색")
 
+class TotalCount(fields.Raw):
+    def format(self, value):
+        return len(value)
+        
 '''text : 텍스트 검색'''
 text_keyord = Search.parser()
 text_keyord.add_argument('keyword', help='검색 키워드', required=True)
@@ -15,6 +19,7 @@ text_liquor = Search.model('text_liquor', {
     'image_path': fields.String(description='Image Path', required=False, example="https://www.berevita.com/pub/media/catalog/product/cache/image/1000x1320/e9c3970ab036de70892d86c6d221abfe/d/e/de-kuyper-blue-curacao.jpg"),
     'description': fields.String(description='Description', required=True, example="오렌지 껍질술인 트리플 섹에 파란 색소를 첨가하여 만든 리큐르. 오렌지 향에 강한 단맛."),
     'rating': fields.Float(description='ration', required=False, example=4.5),
+    'total_bookmark' : TotalCount(description='총 즐겨찾기 수',required=True, attribute='wish_l',example=3),
 })
 
 text_cocktail = Search.model('text_cocktail', {
@@ -23,6 +28,7 @@ text_cocktail = Search.model('text_cocktail', {
     'cocktail_name_kor': fields.String(description='Cocktail name(korean)', required=True, example="블루 하와이"),
     'image_path': fields.String(description='Image Path', required=False, example="https://w.namu.la/s/827ac58e595bb28aa551a1d18fcbcf291a3f01890eb07e4efbb8128eb7007bfdfd0ba10794d4a39194af008f5c7b72c9b6e9386da7a4a0227b19b20884c962d2e2078560c76b2e5a608c4f6dd7b203fdadd31739538aeb5af6c2e26f7b7ac14d"),
     'description': fields.String(description='Description', required=True, example="색이 예쁘다. 새콤달콤한 맛"),
+    'total_bookmark' : TotalCount(description='총 즐겨찾기 수',required=True, attribute='wish_c',example=3),
 })
 
 
