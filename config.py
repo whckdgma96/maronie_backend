6b0655978754e9ddccaf7e7fab438f0be6252498
whckdgma96 <whckdgma96@gmail.com>
2022-03-12 21:03:43 +0900

    config 수정

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 BASE_DIR = os.path.dirname(__file__) # 폴더 구조가 달라져도, 현재 폴더를 가져와서 사용할 수 있도록 설정합니다.
 
 SQLALCHEMY_DATABASE_URI = (f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
-                           f"127.0.0.1:{os.getenv('MYSQL_PORT')}"
+                           f"{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}"
                            f"/{os.getenv('MYSQL_DATABASE')}"
                           )
 
