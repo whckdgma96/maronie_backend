8a6a2b0308e4a8e2f75aae28257cf08842180f45
whckdgma96 <whckdgma96@gmail.com>
2022-03-12 16:39:32 +0900

    로그인 유저 판별 테스트

--- apis/review/reviewService.py
+++ apis/review/reviewService.py
@@ -7,15 +7,12 @@ from db_connect import db
 from datetime import datetime
 
 def create_review(user_id:int,liquor_id:int,rating:float,content:str):
-    print(session["login"])
     logined_user = User.query.filter_by(email=session['login']).first()
-    print(logined_user)
     review_date = datetime.today().strftime("%Y-%m-%d")
-    # print(review_date)
     review_check = Review.query.filter_by(user_id=user_id).filter_by(liquor_id=liquor_id).first()
 
-    # if logined_user.id != user_id:
-    #     abort(500, "로그인 정보가 일치하지 않습니다.")
+    if logined_user.id != user_id:
+        abort(500, "로그인 정보가 일치하지 않습니다.")
     if review_check: # 있는 리뷰 -> 업데이트를 해야댐
         abort(500, "이미 등록된 리뷰가 있습니다")
     else: 
